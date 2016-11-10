ddc0c8334b33c65a0c27e1d73b37317c4330182c
AhmedNY <ahmed.n.yagoub@gmail.com>
2016-11-10 19:35:28 +0300

    - add seeds permissions to match groups - added sql views raw migrations

--- scripts/db_init.sh
+++ scripts/db_init.sh
@@ -1,7 +1,7 @@
 echo "--- droping database if exsists"
-mysql -u root -e 'DROP DATABASE IF EXISTS um'
+mysql -u root -p -e 'DROP DATABASE IF EXISTS um'
 echo "--- creating database"
-mysql -u root -e 'CREATE DATABASE um DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci'
+mysql -u root -p -e 'CREATE DATABASE um DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci'
 
 
 knex migrate:rollback
--- seeds/init_seeds.js
+++ seeds/init_seeds.js
@@ -1,18 +1,16 @@
 exports.seed = function(knex, Promise) {
     return Promise.all([
-        knex('um_permission').insert({id: 1, action: 'manage', resource: 'category'}),
-        knex('um_permission').insert({id: 2, action: 'manage', resource: 'queue'}),
-        knex('um_permission').insert({id: 3, action: 'manage', resource: 'status'}),
-        knex('um_permission').insert({id: 4, action: 'manage', resource: 'post'}),
+        knex('um_permission').insert({id: 1, action: 'manage', resource: 'system'}),
+        knex('um_permission').insert({id: 2, action: 'manage', resource: 'group'}),
 
         knex('um_role').insert({id: 1, role_name: 'Administrator'}),
+        knex('um_role').insert({id: 2, role_name: 'GroupAdmin'}),
         
         knex('um_role_permission').insert({role_id:1, permission_id: 1}),
-        knex('um_role_permission').insert({role_id:1, permission_id: 2}),
-        knex('um_role_permission').insert({role_id:1, permission_id: 3}),
-        knex('um_role_permission').insert({role_id:1, permission_id: 4}),
+        knex('um_role_permission').insert({role_id:2, permission_id: 2}),
 
-        knex('um_badge').insert({user_id: 1, role_id: 1})
+        knex('um_badge').insert({user_id: 1, role_id: 1}),
+        knex('um_badge').insert({user_id: 2, role_id: 2, context:'group', context_id:2}),
     ]).then(function(result){
         console.log('Done');
     });;
